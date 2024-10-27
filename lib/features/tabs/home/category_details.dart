96c16b5eb02d84facb0e342894880749e1d59f39
sagar <sagaryadav12369@gmail.com>
2024-10-27 16:44:36 +0530

    cat

--- lib/features/tabs/home/category_details.dart
+++ lib/features/tabs/home/category_details.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/features/tabs/home/home_screen.dart';
+import 'package:e_commerce_app/utils/constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +9,80 @@ class CategoryDetailsItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: whiteCOlor,
       appBar: AppBar(
-        
+        backgroundColor: whiteCOlor,
+        title: const Text(
+          'Hoodies (240)',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.only(top: 20, right: 5, bottom: 20, left: 20),
+        child: ProductList(products: [
+          Product(
+              imagePath: 'assets/images/products/product_1.png',
+              title: 'product 1',
+              price: '\$29.99',
+              crossPrice: '\$10.00'),
+          Product(
+              imagePath: 'assets/images/products/product_2.png',
+              title: 'product 1',
+              price: '\$29.99',
+              crossPrice: '\$20.00'),
+          Product(
+              imagePath: 'assets/images/products/product_3.png',
+              title: 'product 3',
+              price: '\$29.99',
+              crossPrice: '\$30.00'),
+          Product(
+              imagePath: 'assets/images/products/product_4.png',
+              title: 'product 4',
+              price: '\$29.99',
+              crossPrice: '\$40.00'),
+          Product(
+              imagePath: 'assets/images/products/product_5.png',
+              title: 'product 5',
+              price: '\$29.99',
+              crossPrice: '\$50.00'),
+          Product(
+              imagePath: 'assets/images/products/product_6.png',
+              title: 'product 6',
+              price: '\$29.99',
+              crossPrice: '\$60.00'),
+        ]),
+      ),
+    );
+  }
+}
+
+class ProductsList extends StatelessWidget {
+  final List<Product> products;
+  const ProductsList({super.key, required this.products});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 40,
+        mainAxisSpacing: 10,
+        childAspectRatio: 7,
+      ),
+      itemCount: products.length,
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return ProductCard(
+            imagePath: product.imagePath,
+            title: product.title,
+            price: product.price,
+            crossPrice: product.crossPrice);
+      },
     );
   }
 }
