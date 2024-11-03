3a2d75c8aa70686de8f0f53d379e4b2a3177dd5d
sagar <sagaryadav12369@gmail.com>
2024-11-03 12:05:07 +0530

    today

--- lib/features/tabs/home/product_screen.dart
+++ lib/features/tabs/home/product_screen.dart
@@ -1,6 +1,5 @@
 import 'package:e_commerce_app/utils/constant.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -27,13 +26,13 @@ class ProductScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(20),
+          padding: const EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ProductSlide(),
-              SizedBox(width: 30),
-              Text(
+              const ProductSlide(),
+              const SizedBox(width: 30),
+              const Text(
                 "Men's Harrington jacket",
                 style: TextStyle(
                   color: Colors.black,
@@ -41,7 +40,7 @@ class ProductScreen extends StatelessWidget {
                   fontSize: 18,
                 ),
               ),
-              Text(
+              const Text(
                 "\$148",
                 style: TextStyle(
                   color: primaryColor,
@@ -49,10 +48,43 @@ class ProductScreen extends StatelessWidget {
                   fontSize: 18,
                 ),
               ),
-              SizedBox(width: 20),
+              const SizedBox(width: 20),
               Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 decoration: BoxDecoration(
+                  color: textFiledColor,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                  const Text(
+                      'size',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: (){
 
+                      },
+                      child: const Row(
+                        children:  [
+                        Text(
+                            'L',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Icon(Iconsax.arrow_down_1_copy)
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
